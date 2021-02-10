c881d52894dc81babb248a980148111737997b15
methlouthisami <methlouthisami@users.noreply.github.com>
2021-02-10 17:04:49 +0100

    version 3

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let searchQuery = '';
 const SearchResultDiv = document.querySelector('.serach-result');
 const search = document.querySelector('Form');
@@ -7,169 +6,44 @@ search.addEventListener('submit', (e) => {
     searchQuery = e.target.querySelector('input').value;
     fetchApi();
 })
-async function fetchApi() {
+function fetchApi() {
     const url = `https://randomuser.me/api/?results=10&q=${searchQuery}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    generateHTML(data.results);
-    console.log(data);
+    fetch(url).then((response) =>
+        response.json().then((data) => {
+            console.log(data);
+            generateHTML(data.results);
+
+        })
+    )
+
 
 }
 
 
 function generateHTML(results) {
     let generatedHTML = '';
-    results.map(result => {//on va mapper (loop)le resultat et pour chaque resultat on va créer html item 
+    results.map(result => {
         generatedHTML +=
 
+
+
+
             `
-             <div class="item">
-            
-            <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-       <th scope="col">gender</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td> <h2 class="title">${result.name.first}</h2></td>
-      <td>  <h2 class="title">${result.name.last}</h2></td>
-      <td> <p class="item-data">${result.email}</p></td>
-      <td> <h3 class="title2">${result.gender}</h3></td>
-       <td> <h3 class="title2">${result.login}</h3></td>
-    </tr>
-    </table>
-  
                  
-                    
-                 </div>
+
+
+  <div class="card" style="width: 18rem;">
+  <img src="${result.picture.large}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${result.name.first}+${result.name.last}</h5>
+    <p class="card-text">${result.email}</p>
+     <p class="card-text">${result.gender}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
                 
    `
     })
     SearchResultDiv.innerHTML = generatedHTML;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*let myInput = document.querySelector("user");
-let getButton = document.querySelector("but");
-let data = document.querySelector("div");
-
-getButton.onclick = function () {
-    semi();
-}
-
-
-function semi() {
-    fetch("https://randomuser.me/api/?results=10")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            for (let i = 0; i < data.results; i++) {
-                text += data.results[i]; $(repo.name) + "<br>";
-            }
-
-            document.getElementById("map").innerHTML = text;
-
-        })
-
-
-}
- .catch (err => (console.log(err)))
-
-
-
-*/
