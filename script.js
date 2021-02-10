2823786039078cd840a882b5cd5ea2f8d0a8843a
methlouthisami <methlouthisami@users.noreply.github.com>
2021-02-10 12:29:08 +0100

    ajout de javascript

--- script.js
+++ script.js
@@ -1,4 +1,150 @@
-let myInput = document.querySelector("user");
+
+let searchQuery = '';
+const SearchResultDiv = document.querySelector('.serach-result');
+const search = document.querySelector('Form');
+search.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchQuery = e.target.querySelector('input').value;
+    fetchApi();
+})
+async function fetchApi() {
+    const url = `https://randomuser.me/api/?results=10&q=${searchQuery}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    generateHTML(data.results);
+    console.log(data);
+
+}
+
+
+function generateHTML(results) {
+    let generatedHTML = '';
+    results.map(result => {//on va mapper (loop)le resultat et pour chaque resultat on va créer html item 
+        generatedHTML +=
+
+            `
+             <div class="item">
+            
+            <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+       <th scope="col">gender</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td> <h2 class="title">${result.name.first}</h2></td>
+      <td>  <h2 class="title">${result.name.last}</h2></td>
+      <td> <p class="item-data">${result.email}</p></td>
+      <td> <h3 class="title2">${result.gender}</h3></td>
+       <td> <h3 class="title2">${result.login}</h3></td>
+    </tr>
+    </table>
+  
+                 
+                    
+                 </div>
+                
+   `
+    })
+    SearchResultDiv.innerHTML = generatedHTML;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let myInput = document.querySelector("user");
 let getButton = document.querySelector("but");
 let data = document.querySelector("div");
 
@@ -22,13 +168,8 @@ function semi() {
 
 
 }
+ .catch (err => (console.log(err)))
 
 
 
-
-
-    .catch (err => (console.log(err)))
-
-
-
-
+*/
